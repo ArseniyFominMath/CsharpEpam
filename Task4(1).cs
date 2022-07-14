6ad17a87d62bd261c7084c9e628801b93f79f144
ArseniyFominMath <arseniyif@gmail.com>
2022-07-14 20:17:02 +0300

    task10IfElseStatements

---
+++ Task4(1).cs
@@ -0,0 +1,45 @@
+﻿namespace IfStatements
+{
+    public static class Task4
+    {
+        public static int DoSomething(bool b1, bool b2, bool b3, bool b4)
+        {
+            int result = 0;
+
+            if ((b2 || !b3) && b4 && b1)
+            {
+                result = 1;
+            }
+            else if (b1 && (b2 || !b3) && !b4)
+            {
+                result = 2;
+            }
+            else if (b1 && !b2 && b3 && !b4)
+            {
+                result = 3;
+            }
+            else if (b1 && !b2 && b3 && b4)
+            {
+                result = 4;
+            }
+            else if (!b1 && !b3 && (b2 || !b4))
+            {
+                result = 5;
+            }
+            else if (!b1 && b3 && (b2 || !b4))
+            {
+                result = 6;
+            }
+            else if (!b1 && !b2 && b3 && b4)
+            {
+                result = 7;
+            }
+            else if (!b1 && !b2 && !b3 && b4)
+            {
+                result = 8;
+            }
+
+            return result;
+        }
+    }
+}
