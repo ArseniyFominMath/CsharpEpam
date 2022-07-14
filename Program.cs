9e34842b90f99b5753b7bc87bd41538e13a7fb88
ArseniyFominMath <arseniyif@gmail.com>
2022-07-14 20:19:30 +0300

    task13PrintFace

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrintFace
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("  Hello, world! ");
+        }
+
+        public static void SayHelloUser(string userName)
+        {
+            Console.WriteLine("Hello, " + userName + "!");
+        }
+
+        public static void PrintFace()
+        {
+            string face = " +\"\"\"\"\"+\n" +
+                              "(| o o |)\n" +
+                              " |  ^  |\n" +
+                              " | '-' |\n" +
+                              " +-----+";
+            Console.WriteLine(face);
+        }
+    }
+}
